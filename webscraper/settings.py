82a4bde134f003828ceadfbec6e12d53cbb1449a
stany <2001stany@gmail.com>
2020-05-12 06:47:58 -0400

    prepare for deployment

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -1,5 +1,7 @@
 
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,7 +16,7 @@ SECRET_KEY = 'j2rq^0p(6lk1y$y*@2hw-482ry2+apkzu@ir)tcr5*3kb6u-de'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'debugoverflow.herokuapp.com']
 
 
 # Application definition
@@ -108,3 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
