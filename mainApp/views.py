d70e64dce33de5fe8154be36787274650cf50749
stany <2001stany@gmail.com>
2020-05-12 16:18:43 -0400

    debuging

--- mainApp/views.py
+++ mainApp/views.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 # GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
-# CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 # GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
 
 chrome_options = webdriver.ChromeOptions()
@@ -32,7 +32,7 @@ def index(request):
         obj = Search()
         obj.word = word
         print(word)
-        driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
         # driver = webdriver.Chrome(ChromeDriverManager().install())
         driver.maximize_window()
@@ -48,7 +48,6 @@ def index(request):
             link.send_keys(Keys.CONTROL + 't', Keys.RETURN)
             
     return render(request, template_name='index.html')
-
 
 
 
