ac62f613dd9e033a4eb19a8ffcc436e8d9445ee0
stany <2001stany@gmail.com>
2020-05-12 07:55:09 -0400

    damn! debuging

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_FILES = [os.path.join(BASE_DIR, 'staticfiles')]
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
