db3a01da7b546629a763a5c9f9ff80a4b32e498b
Stanley Ruheza <48031010+maen08@users.noreply.github.com>
2021-09-13 10:46:06 +0300

    neat

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,11 +5,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
-# GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-# CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-# GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
@@ -17,13 +14,6 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-
-# driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
-# driver.get('https://stackoverflow.com/')
-
-
-# driver.quit()
-
 
 
 def index(request):
